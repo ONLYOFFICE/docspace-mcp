57a56174e21dfefd9f0f85e0ef60feb486f69f10
vanyauhalin <ivan.uhalin@onlyoffice.com>
2025-06-09 12:33:03 +0400

    add fields filter

--- lib/server.ts
+++ lib/server.ts
@@ -39,6 +39,7 @@ import {
 	GetFolderInfoInputSchema,
 	GetFolderInputSchema,
 	GetFoldersInputSchema,
+	GetMyFolderInputSchema,
 	GetRoomInfoInputSchema,
 	GetRoomSecurityInfoInputSchema,
 	GetRoomsFolderInputSchema,
@@ -147,7 +148,7 @@ export class Server {
 				{
 					name: "files_get_my_folder",
 					description: "Get the 'My Documents' folder.",
-					inputSchema: toInputSchema(z.object({})),
+					inputSchema: toInputSchema(GetMyFolderInputSchema),
 				},
 				{
 					name: "files_copy_batch_items",
@@ -285,7 +286,7 @@ export class Server {
 				cr = await this.files.renameFolder(extra.signal, req.params.arguments)
 				break
 			case "files_get_my_folder":
-				cr = await this.files.getMyFolder(extra.signal)
+				cr = await this.files.getMyFolder(extra.signal, req.params.arguments)
 				break
 			case "files_copy_batch_items":
 				cr = await this.files.copyBatchItems(extra.signal, req.params.arguments)

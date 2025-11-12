ffcdb70a0a8514a42eb53dd897a16e2e4bb8f6ec
vanyauhalin <ivan.uhalin@onlyoffice.com>
2025-11-12 21:24:57 +0400

    update sse

--- lib/mcp/sse-server.ts
+++ lib/mcp/sse-server.ts
@@ -21,41 +21,44 @@
  * @mergeModuleWith mcp
  */
 
+/* eslint-disable typescript/consistent-type-definitions */
+
 import type * as server from "@modelcontextprotocol/sdk/server/index.js"
 import type * as sse from "@modelcontextprotocol/sdk/server/sse.js"
-import cors from "cors"
 import express from "express"
-import * as expressRateLimit from "express-rate-limit"
 import * as errors from "../util/errors.ts"
+import * as utilExpress from "../util/express.ts"
 import * as result from "../util/result.ts"
 
-export interface SseServerConfig {
+export type SseServerConfig = {
 	corsOrigin: string[]
 	corsMaxAge: number
 	corsAllowedHeaders: string[]
 	corsExposedHeaders: string[]
 	rateLimitCapacity: number
 	rateLimitWindow: number
+	handlers: express.Handler[]
 	servers: SseServerServers
 	transports: SseServerTransports
 }
 
-export interface SseServerServers {
+export type SseServerServers = {
 	create(req: express.Request): result.Result<server.Server, Error>
 }
 
-export interface SseServerTransports {
+export type SseServerTransports = {
 	create(endpoint: string, res: express.Response): sse.SSEServerTransport
 	retrieve(id: string): result.Result<sse.SSEServerTransport, Error>
 }
 
-class SseServer {
+export class SseServer {
 	private corsOrigin: string[]
 	private corsMaxAge: number
 	private corsAllowedHeaders: string[]
 	private corsExposedHeaders: string[]
 	private rateLimitCapacity: number
 	private rateLimitWindow: number
+	private handlers: express.Handler[]
 	private servers: SseServerServers
 	private transports: SseServerTransports
 
@@ -66,69 +69,97 @@ class SseServer {
 		this.corsExposedHeaders = config.corsExposedHeaders
 		this.rateLimitCapacity = config.rateLimitCapacity
 		this.rateLimitWindow = config.rateLimitWindow
+		this.handlers = config.handlers
 		this.servers = config.servers
 		this.transports = config.transports
 	}
 
-	cors(): express.Handler {
-		if (this.corsOrigin.length !== 0) {
-			return (_, __, next) => {
-				next()
+	router(): express.Router {
+		// todo: add recovery middleware
+		// todo: add signal middleware
+		// todo: add allowedMethods middleware
+		// todo: add supportedMediaTypes middleware
+
+		let cors = (r: express.Router): void => {
+			if (this.corsOrigin.length !== 0) {
+				let co: utilExpress.CorsOptions = {
+					origin: this.corsOrigin,
+					maxAge: this.corsMaxAge,
+					methods: ["GET", "POST"],
+					allowedHeaders: [
+						...this.corsAllowedHeaders,
+						"Content-Type",
+						"Mcp-Session-Id",
+					],
+					exposedHeaders: [
+						...this.corsExposedHeaders,
+						"Mcp-Session-Id",
+					],
+				}
+
+				if (this.rateLimitCapacity && this.rateLimitWindow) {
+					co.exposedHeaders.push(...utilExpress.rateLimitHeaders)
+				}
+
+				r.use(utilExpress.cors(co))
 			}
 		}
 
-		let o: cors.CorsOptions = {
-			origin: this.corsOrigin,
-			methods: ["GET", "POST"],
-			allowedHeaders: [
-				...this.corsAllowedHeaders,
-				"Content-Type",
-				"Mcp-Session-Id",
-			],
-		}
+		let guard = (r: express.Router): void => {
+			if (this.rateLimitCapacity && this.rateLimitWindow) {
+				let er = new errors.
+					MessageError("Too many requests, please try again later").
+					toString()
 
-		let exposedHeaders: string[] = [
-			...this.corsExposedHeaders,
-			"Mcp-Session-Id",
-		]
+				let ro: utilExpress.RateLimitOptions = {
+					capacity: this.rateLimitCapacity,
+					window: this.rateLimitWindow,
+				}
 
-		if (this.rateLimitCapacity && this.rateLimitWindow) {
-			exposedHeaders.push(
-				"Retry-After",
-				"RateLimit-Limit",
-				"RateLimit-Remaining",
-				"RateLimit-Reset",
-			)
-		}
-
-		o.exposedHeaders = exposedHeaders
-
-		if (this.corsMaxAge) {
-			o.maxAge = this.corsMaxAge / 1000
-		}
-
-		return cors(o)
-	}
-
-	rateLimit(): express.Handler {
-		if (!this.rateLimitCapacity || !this.rateLimitWindow) {
-			return (_, __, next) => {
-				next()
+				r.use(utilExpress.rateLimit(ro, (_, res) => {
+					res.json(er)
+				}))
 			}
 		}
 
-		return expressRateLimit.rateLimit({
-			windowMs: this.rateLimitWindow,
-			limit: this.rateLimitCapacity,
-			standardHeaders: true,
-			legacyHeaders: false,
-			message: new errors.
-				MessageError("Too many requests, please try again later").
-				toString(),
-		})
+		let r = express.Router()
+
+		r.use("/sse", (() => {
+			let r = express.Router()
+
+			r.use(express.json())
+
+			cors(r)
+
+			r.use(this.handlers)
+
+			guard(r)
+
+			r.get("/", this.handleSse.bind(this))
+
+			return r
+		})())
+
+		r.use("/messages", (() => {
+			let r = express.Router()
+
+			r.use(express.json())
+
+			cors(r)
+
+			r.use(this.handlers)
+
+			guard(r)
+
+			r.post("/", this.handleMessages.bind(this))
+
+			return r
+		})())
+
+		return r
 	}
 
-	async sse(req: express.Request, res: express.Response): Promise<void> {
+	private async handleSse(req: express.Request, res: express.Response): Promise<void> {
 		try {
 			let s = this.servers.create(req)
 			if (s.err) {
@@ -161,7 +192,7 @@ class SseServer {
 		}
 	}
 
-	async messages(req: express.Request, res: express.Response): Promise<void> {
+	private async handleMessages(req: express.Request, res: express.Response): Promise<void> {
 		try {
 			let id = req.headers["mcp-session-id"]
 
@@ -216,39 +247,4 @@ class SseServer {
 			}
 		}
 	}
-}
-
-export function sseServer(
-	config: SseServerConfig,
-	...handlers: express.RequestHandler[]
-): express.Router {
-	let s = new SseServer(config)
-
-	let r = express.Router()
-
-	r.use("/sse", ...handlers, (() => {
-		let r = express.Router()
-
-		r.use(express.json())
-		r.use(s.cors())
-		r.use(s.rateLimit())
-
-		r.get("/", s.sse.bind(s))
-
-		return r
-	})())
-
-	r.use("/messages", ...handlers, (() => {
-		let r = express.Router()
-
-		r.use(express.json())
-		r.use(s.cors())
-		r.use(s.rateLimit())
-
-		r.post("/", s.messages.bind(s))
-
-		return r
-	})())
-
-	return r
 }

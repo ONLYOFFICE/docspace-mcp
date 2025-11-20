081bf376dccbfb4b2103b914f4e78687781673ec
vanyauhalin <ivan.uhalin@onlyoffice.com>
2025-11-20 14:05:59 +0400

    return users with all statuses

--- lib/mcp/regular-tools.ts
+++ lib/mcp/regular-tools.ts
@@ -32,10 +32,10 @@ import {
 	FileShareDtoSchema,
 	FolderContentDtoSchema,
 	FolderDtoSchema,
-	GetAllFiltersSchema,
 	GetFileInfoFiltersSchema,
 	GetFolderFiltersSchema,
 	GetFolderInfoFiltersSchema,
+	GetFullByFilterFiltersSchema,
 	GetMyFolderFiltersSchema,
 	GetRoomInfoFiltersSchema,
 	GetRoomSecurityFiltersSchema,
@@ -366,7 +366,7 @@ export const GetRoomAccessLevelsSchema = z.object({
 //
 
 export const GetAllPeopleInputSchema = z.object({
-	filters: GetAllFiltersSchema.describe("The filters to apply to the list of people. Use them to reduce the size of the response."),
+	filters: GetFullByFilterFiltersSchema.describe("The filters to apply to the list of people. Use them to reduce the size of the response."),
 })
 
 export const GetAllPeopleOutputSchema = SuccessApiResponseSchema.extend({
@@ -993,7 +993,7 @@ export class RegularTools {
 			return error(new Error("Parsing input.", {cause: pr.error}))
 		}
 
-		let gr = await this.s.client.people.getAll(signal, pr.data.filters)
+		let gr = await this.s.client.people.getFullByFilter(signal, pr.data.filters)
 		if (gr.err) {
 			return error(new Error("Getting people.", {cause: gr.err}))
 		}

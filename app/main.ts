db6934c4f41c9f69395441d3cb16d3e02e209dcd
vanyauhalin <ivan.uhalin@onlyoffice.com>
2026-02-10 16:37:37 +0400

    apply allowed hostnames to stremable

--- app/main.ts
+++ app/main.ts
@@ -1255,6 +1255,7 @@ function startHttp(config: Config, logger: utilLogger.VanillaLogger): r.Result<S
 		let st = new mcp.StreamableTransports(stc)
 
 		let ssc: mcp.StreamableServerConfig = {
+			allowedHostnames: config.server.allowedHostnames,
 			corsOrigin: config.server.cors.mcp.origin,
 			corsMaxAge: config.server.cors.mcp.maxAge,
 			corsAllowedHeaders: [

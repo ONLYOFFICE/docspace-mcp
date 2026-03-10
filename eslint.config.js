a292e31262e89d46d0a6fade250a99bff7f563cd
vanyauhalin <ivan.uhalin@onlyoffice.com>
2026-03-10 20:01:48 +0400

    remove sdk server class from eslint

--- eslint.config.js
+++ eslint.config.js
@@ -15,7 +15,7 @@ export default [
 			"es-x/no-object-getownpropertysymbols": "off",
 			"import-x/no-deprecated": "off",
 			"stylistic/space-before-function-paren": ["error", {anonymous: "never", asyncArrow: "never", catch: "always", named: "never"}],
-			"typescript/no-deprecated": ["error", {allow: [{from: "package", package: "@modelcontextprotocol/sdk", name: "Server"}, {from: "package", package: "@modelcontextprotocol/sdk", name: "SSEClientTransport"}, {from: "package", package: "@modelcontextprotocol/sdk", name: "SSEServerTransport"}]}],
+			"typescript/no-deprecated": ["error", {allow: [{from: "package", package: "@modelcontextprotocol/sdk", name: "SSEClientTransport"}, {from: "package", package: "@modelcontextprotocol/sdk", name: "SSEServerTransport"}]}],
 		},
 	},
 ]
